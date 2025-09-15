b092cbd88347ac604a154de3287299b0a02ac0e8
Taha Moosavi <taha.moosavi.taha@gmail.com>
2025-09-15 09:29:01 +0330

    Auto commit by Rust tool [git-commit-zenity]

--- src/app/features/dashboard/user-management/user-edit/user-edit.component.ts
+++ src/app/features/dashboard/user-management/user-edit/user-edit.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { MatFormField, MatInput, MatLabel, MatSuffix } from '@angular/material/input';
 
 
 @Component({
   selector: 'app-user-edit',
-  imports: [MatFormField, MatIcon, MatLabel, MatButton, ReactiveFormsModule, MatInput],
+  imports: [MatFormField, MatIcon, MatLabel, MatButton, ReactiveFormsModule, MatInput , MatSuffix] ,
   templateUrl: './user-edit.component.html',
   styleUrl: './user-edit.component.css'
 })
