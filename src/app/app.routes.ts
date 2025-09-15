b91ac5560cfd22b9d8b53467367947ed46a9e700
Taha Moosavi <taha.moosavi.taha@gmail.com>
2025-09-15 09:08:53 +0330

    Auto commit by Rust tool [git-commit-zenity]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { LoginComponent } from './features/auth/login/login.component';
 import { NotFoundComponent } from './features/notfound/notfound.component';
 import { HomeComponent } from './features/home/home.component';
+import { UserManagementComponent } from './features/dashboard/user-management/user-management.component';
+import { UserEditComponent } from './features/dashboard/user-management/user-edit/user-edit.component';
+import { UserAddComponent } from './features/dashboard/user-management/user-add/user-add.component';
 
 export const routes: Routes = [
   {
@@ -13,6 +16,18 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+  },
+  {
+    path: 'dashboard/users',
+    component: UserManagementComponent,
+  },
+  {
+    path: 'dashboard/users/add',
+    component:UserAddComponent
+  },
+  {
+    path: 'dashboard/users/:id',
+    component: UserEditComponent
   },
   {
     path: 'login',
